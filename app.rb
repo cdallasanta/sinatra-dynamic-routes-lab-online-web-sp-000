0445ec49ed4fc6eaa44e2449c71bdbaab41f6b53
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-24 18:32:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ class App < Sinatra::Base
   get '/square/:number' do
     "#{params[:number].to_i * params[:number].to_i}"
   end
-
+=begin
   get '/say/:number/:phrase' do
     binding.pry
     params[:number].to_i.times do
       "#{params[:phrase]}"
     end
   end
-
+=end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
