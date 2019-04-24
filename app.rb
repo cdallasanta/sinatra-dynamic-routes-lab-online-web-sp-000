5fa1f5bfbef8ab3e3440e35cf4fa63b927f2551a
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-24 18:13:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do
 
+  end
+
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
 end
