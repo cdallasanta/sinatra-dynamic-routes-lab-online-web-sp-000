4d0ddfaee93e6d77e79262534d4e891517e95544
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-24 18:29:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    "#{params[:number]*params[:number]}"
+    binding.pry
+    "#{params[:number] * params[:number]}"
   end
 
   get '/say/:number/:phrase' do
